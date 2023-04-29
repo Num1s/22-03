b4e9b31ee845039405cf3e52d44113cfc62bb4ae
sst1mxx <nbsultanbekov@gmail.com>
2023-04-29 16:44:03 +0600

    DZ4

--- src/store/userReducer.js
+++ src/store/userReducer.js
@@ -13,15 +13,19 @@ export const userReducer = (state=initialState, action) => {
         case CHANGE_USER:
             if (action.payload.user.age < 10 || action.payload.user.age > 100) {
                 alert(`Вам нет 10-ти или вам больше 100, доступ запрещен`)
+                break;
             }
             if (action.payload.user.name === undefined) {
                 alert(`Укажите имя!`)
+                break;
             }
             if (action.payload.user.age === undefined) {
                 alert(`Укажите возвраст!`)
+                break;
             }
             if (action.payload.user.gender === undefined) {
                 alert(`Укажите пол!`)
+                break;
             }
             return {...state, user: action.payload.user}
         default:
